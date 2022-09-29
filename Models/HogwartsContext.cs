e51c9c246271fce8525636315c844ee5f03e7385
szanil <szaniszlolaszlo95@gmail.com>
2022-09-29 13:55:08 +0200

    Update HogwartsContext.cs
    Added dbContexts

--- Models/HogwartsContext.cs
+++ Models/HogwartsContext.cs
@@ -9,6 +9,11 @@ namespace HogwartsPotions.Models
     public class HogwartsContext : DbContext
     {
         public const int MaxIngredientsForPotions = 5;
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Potion> Potions { get; set; }
 
         public HogwartsContext(DbContextOptions<HogwartsContext> options) : base(options)
         {
