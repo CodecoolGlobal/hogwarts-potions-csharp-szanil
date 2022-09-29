912db55f6305dc5b42d3ee437c8750496333a7b7
szanil <szaniszlolaszlo95@gmail.com>
2022-09-29 13:47:10 +0200

    Create DbInitializer.cs

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HogwartsPotions.Models;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Data;
+
+public class DbInitializer
+{
+    public static void Initialize(HogwartsContext context)
+    {
+        context.Database.EnsureCreated();
+
+        // Look for any students.
+        if (context.Students.Any())
+        {
+            return;   // DB has been seeded
+        }
+}
