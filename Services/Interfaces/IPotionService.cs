c191d62246d27b44babb5535b035df9af596cdcb
szanil <szaniszlolaszlo95@gmail.com>
2022-09-29 13:54:05 +0200

    Create IPotionService.cs

---
+++ Services/Interfaces/IPotionService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models.Entities;
+
+namespace HogwartsPotions.Services.Interfaces
+{
+    public interface IPotionService
+    {
+        Task<List<Potion>> GetAllPotions();
+        Task<List<Potion>> GetPotionByStudentId(long studentId);
+        Task<Potion> BrewingPotion(Potion potion);
+        Task<Potion> BrewFreshPotion(Potion potion);
+        Task<Potion> AddIngredient(long potionId, Ingredient ingredient);
+        Task<List<Recipe>> SimilarRecipes(long potionId);
+        Task<Potion> GetPotion(long potionId);
+        Task<List<Recipe>> GetAllRecipe();
+    }
+}
