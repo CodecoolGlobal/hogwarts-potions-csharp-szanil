0b23d8cc17f5e876eb4b98e6d976950f4b05eb43
szanil <szaniszlolaszlo95@gmail.com>
2022-09-29 13:56:17 +0200

    Create Ingredient.cs

---
+++ Models/Entities/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace HogwartsPotions.Models.Entities;
+
+public class Ingredient
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
+    public string Name { get; set; }
+    [JsonIgnore]
+    public HashSet<Potion> Potions { get; set; }
+    [JsonIgnore]
+    public HashSet<Recipe> Recipes { get; set; }
+}
