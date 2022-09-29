73b8e99004378a10a12cd0fc9d12741aaf1878cf
szanil <szaniszlolaszlo95@gmail.com>
2022-09-29 13:57:16 +0200

    Create PotionsController.cs

---
+++ Controllers/PotionsController.cs
@@ -0,0 +1,12 @@
+﻿using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HogwartsPotions.Controllers
+{
+    [ApiController, Route("/potions")]
+    public class PotionsController : Controller
+    {
+    }
+}
