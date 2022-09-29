e1c4aa19281d48d8e0eb3b35264f5db975054649
szanil <szaniszlolaszlo95@gmail.com>
2022-09-29 13:56:21 +0200

    Create Potion.cs

---
+++ Models/Entities/Potion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Models.Entities;
+
+public class Potion
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
+    public string Name { get; set; }
+    public Student Student { get; set; }
+    public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public BrewingStatus BrewingStatus { get; set; } = BrewingStatus.Brew;
+    public Recipe Recipe { get; set; }
+}
